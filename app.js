23c46596ef7d31bc5fdf1eeed99f0e9a7d4c8840
tibraj <tibraj94@gmail.com>
2021-01-08 14:13:55 -0500

    syntax

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.getElementById('minimum').addEventListener('click', minimumTest);
 
 function minimumTest(a, b) {
-    a < b ? a : b;
+    if(a < b) return a;
+    else return b;
 }
+
+console.log(minimumTest(23, 30));
